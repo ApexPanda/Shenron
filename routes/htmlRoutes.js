bf3a5cd05c71f93799d959a1db34271bf2e5b779
mountainflow <46331541+mountainflow@users.noreply.github.com>
2019-05-18 12:45:22 -0600

    changed html routes to update meta tags

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,18 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
+      // This will load title and description for each page separately=================================
+      res.locals.metaTags = {
+        title: "Fur Butlr",
+        description: "A place where pet owners can find all thier needs in one place!",
+        keywords: "pet grooming, pet sitting, pet walking"
+      };
       res.render("index", {
+        layout: "main",
         msg: "Welcome!",
         examples: dbExamples
       });
@@ -12,8 +20,8 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (
       dbExample
     ) {
       res.render("example", {
@@ -23,7 +31,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };

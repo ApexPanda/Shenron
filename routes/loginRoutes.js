4330aeccd845be4e2207adffe0b21cdee4335127
emssslay <emily.robinson@iechs.org>
2019-05-18 11:36:13 -0600

    writing for login is done, now to test

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 exports.register = function (req, res) {
-  // console.log("req", req.body);
+  console.log("req", req.body);
   var today = new Date();
   var users = {
     "first_name": req.body.first_name,
@@ -10,19 +10,44 @@ exports.register = function (req, res) {
     "created": today,
     "modified": today
   };
-  RTCPeerConnection.query("INSERT INTO users SET ?", users, function (err, res, fields) {
-    if (err) {
-      console.log("error ocurred", err);
+  RTCPeerConnection.query("INSERT INTO users_table SET ?", users, function (error, results, fields) {
+    if (error) {
+      console.log("Error ocurred", error);
       res.send({
         "code": 400,
-        "failed": "login error occured"
+        "failed": "Registration error occured"
       });
     } else {
       console.log("The solution is: ", results);
       res.send({
         "code": 200,
-        "success": "user registered successfully"
+        "success": "User registered successfully"
       });
     }
+    console.log(fields);
   });
+};
+
+exports.login = function (req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+  connection.query("SELECT * FROM users_table WHERE email = ?", [email],
+    function (error, results, fields) {
+      if (error) {
+        console.log("Error ocurred", error);
+        res.send({
+          "code": 400,
+          "failed": "Login error ocurred"
+        });
+      } else {
+        console.log("The solution is: ", results);
+        if (results[0].password == password) {
+          res.send({
+            "code": 200,
+            "success": "Login Successful"
+          });
+        }
+      }
+      console.log(fields);
+    });
 };

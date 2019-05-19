8c545de053533cfec15af135175d163063642e55
emssslay <emily.robinson@iechs.org>
2019-05-19 15:26:43 -0600

    changed html routes from example filler

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,21 +3,21 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.users_table.findAll({}).then(function(dbUsers) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        user: dbUsers
       });
     });
   });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
+    db.users_table.findOne({ where: { id: req.params.id } }).then(function(
+      dbUser
     ) {
-      res.render("example", {
-        example: dbExample
+      res.render("user", {
+        user: dbUser
       });
     });
   });

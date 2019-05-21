8178bd288c48a8d5cc604c117866fedd29c76b07
emssslay <emily.robinson@iechs.org>
2019-05-20 18:28:23 -0600

    fixing issue for travis

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,6 @@ var db = require("../models");
 
 module.exports = function (app) {
   // Load index page
-
   app.get("/", function (req, res) {
     // This will load title and description for each page separately=================================
     res.locals.metaTags = {
@@ -56,13 +55,12 @@ module.exports = function (app) {
   });
 
   // Load example page and pass in an example by id
-
   app.get("/example/:id", function (req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function (
       dbExample
     ) {
-      res.render("user", {
-        user: dbUser
+      res.render("example", {
+        example: dbExample
       });
     });
   });

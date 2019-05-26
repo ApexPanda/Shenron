83f312fd7408cfead87d08aa3fe816c4f803917a
emssslay <emily.robinson@iechs.org>
2019-05-26 15:33:30 -0600

    added pets schema

--- models/schema.sql
+++ models/schema.sql
@@ -20,5 +20,20 @@ CREATE TABLE `users` (
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+CREATE TABLE `pets` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `pet_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+ `owner_id` int(11) NOT NULL,
+ `pet_type` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+ `image` varbinary(200),
+ `about_me` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
+ `location` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+ `created` datetime NOT NULL,
+ `modified` datetime NOT NULL,
+ PRIMARY KEY (`id`),
+ FOREIGN KEY(owner_id) REFERENCES users(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
 USE fur_db;
 DROP TABLE IF EXISTS examples;

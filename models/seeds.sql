ad8a37ac64dff28cb9eb56adfa24a1572425b2f0
emssslay <emily.robinson@iechs.org>
2019-05-26 15:06:45 -0600

    added to seeds

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,7 +1,7 @@
 --
 USE fur_db;
 
-INSERT INTO users(first_name, last_name, role, email, password, created, modified) VALUES ("Betsy", "Smith", "Pet Owner", "betsy.smith@gmail.com", "password234", "2019-05-01 12:00:00", "2019-05-01 12:00:00");
-INSERT INTO users(first_name, last_name, role, email, password, created, modified) VALUES ("John", "Hammerford", "Salon Owner", "anemail@yahoo.com", "passwor234234", "2019-05-01 12:00:00", "2019-05-01 12:00:00");
-INSERT INTO users(first_name, last_name, role, email, password, created, modified) VALUES ("Lauren", "Oswald", "Dog Walker", "unicornFlower@gmail.com", "passworsdfs", "2019-05-01 12:00:00", "2019-05-01 12:00:00");
-INSERT INTO users(first_name, last_name, role, email, password, created, modified) VALUES ("Billy", "McMann", "Pet Owner", "billMc@aol.com", "passwordo7yuk", "2019-05-01 12:00:00", "2019-05-01 12:00:00");
+INSERT INTO users(first_name, last_name, role, email, password, about_me, location, created, modified) VALUES ("Betsy", "Smith", "Pet Owner", "betsy.smith@gmail.com", "password234", "I love pets!", "Provo, Utah", "2019-05-01 12:00:00", "2019-05-01 12:00:00");
+INSERT INTO users(first_name, last_name, role, email, password, about_me, location, created, modified) VALUES ("John", "Hammerford", "Salon Owner", "anemail@yahoo.com", "passwor234234", "I love dogs!", "Orem, Utah", "2019-05-01 12:00:00", "2019-05-01 12:00:00");
+INSERT INTO users(first_name, last_name, role, email, password, about_me, location, created, modified) VALUES ("Lauren", "Oswald", "Dog Walker", "unicornFlower@gmail.com", "passworsdfs", "I love cats!", "Salt Lake City, Utah", "2019-05-01 12:00:00", "2019-05-01 12:00:00");
+INSERT INTO users(first_name, last_name, role, email, password, about_me, location, created, modified) VALUES ("Billy", "McMann", "Pet Owner", "billMc@aol.com", "passwordo7yuk", "I love animals!", "Taylorsville, Utah", "2019-05-01 12:00:00", "2019-05-01 12:00:00");
